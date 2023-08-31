10efd0ddcca6d5fbbb02dfd7ab2bbc1d8580a3d9
Zineb49 <lahrizizineb1@gmail.com>
2023-08-31 13:57:18 -0700

    flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -16,7 +16,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	for (c = 63; c >= 0; c--)
 	{
-		current = exclusive >> i;
+		current = exclusive >> c;
 		if (current & 1)
 			cnt++;
 	}
