dd3087c44b61ea5e3d8e1e8dce1069871cd490d4
Zineb49 <lahrizizineb1@gmail.com>
2023-09-04 12:39:05 -0700

    append txt to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	op = open(filename, O_WRONLY | O_APPEND);
-	wi = write(o, text_content, len);
+	wi = write(op, text_content, len);
 
 	if (op == -1 || wi == -1)
 		return (-1);
